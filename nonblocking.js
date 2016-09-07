470132b34e48b4e94b19e83433bbd5771095b73f
Naga Rimmalapudi <Naga Rimmalapudi>
2016-09-07 13:58:17 -0400

    This is my first class of node.js

--- nonblocking.js
+++ nonblocking.js
@@ -11,7 +11,7 @@ console.log(food);
 
 console.log('FOOD: ');
 
-var drinks = fs.readFile('food.txt', 'utf8', function(err, food){
+var drinks = fs.readFile('drinks.txt', 'utf8', function(err, drinks){
     console.log(drinks);
 });
 
